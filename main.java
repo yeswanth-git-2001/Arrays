de1592ce9d5268c96ebc6e601c9cffa4860f4565
yeswanth <yeswanthchunchu@gmail.com>
2024-05-08 11:51:11 +0530

    main.java uploaded

---
+++ main.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        Arrays arrays = new Arrays();
+        arrays.oneDimenArray();
+        arrays.showOneDimenArray();
+        arrays.twoDimenRegularArray();
+        arrays.displayTwoDimenRegularArray();
+        arrays.twoDimenIregularArray();
+        arrays.displayTwoDimenIregularArray();
+        arrays.threeDimenRegularArrya();
+        arrays.displayThreeDimenRegukarArray();
+        arrays.threeDimenIregularArrya();
+        arrays.displayThreeDimenIregukarArray();
+        arrays.arrayOfObjects();
+        arrays.enhancedForLoop();
+    }
+}
